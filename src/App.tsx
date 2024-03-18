e28332c0d2c268304442f7a6d6fee9c9b3627f22
Sidnei <sidneisonic@hotmail.com>
2024-03-18 11:10:53 -0300

    feat: apenas verificando o component Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import Layout from "./components/Layout";
 import GlobalStyles from "./styles/GlobalStyles";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <h1 style={{color: "red"}}>My App</h1>
-      </div>
+      <Layout />
     </>
 
   );
