9145ec44b06d2d33a3864dcb1de560c3939739f2
Sidnei <sidneisonic@hotmail.com>
2024-03-18 12:15:04 -0300

    feat: criados os elementos no styles e adicionando no MainHeader

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles";
+import { useMemo } from "react";
+import { Container, Profile, Username, Welcome } from "./styles";
+import emojis from "../../utils/emojis";
 
 const MainHeader = () => {
+
+    const emoji = useMemo(() => {
+        const index = Math.floor(Math.random() * emojis.length);
+        return emojis[index];
+    }, []);
+
     return (
         <Container>
             <h1>MainHeader</h1>
+
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <Username>Sidnei de Souza Junior</Username>
+            </Profile>
         </Container>
     );
 }
