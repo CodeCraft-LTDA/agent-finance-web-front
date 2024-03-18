409d2e8524bdf1ee80be82221098155f58113559
Sidnei <sidneisonic@hotmail.com>
2024-03-18 16:33:25 -0300

    feat: adicionado contentheader e selectinput no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
 import { Container } from "./styles";
 
 const Dashboard = () => {
     return (
         <Container>
-            <ContentHeader />
+            <ContentHeader title="Dashboard" lineColor="#fff">
+                <SelectInput options={[{value: 'teste', label: 'teste'}]} />
+            </ContentHeader>
         </Container>
     );
 };
