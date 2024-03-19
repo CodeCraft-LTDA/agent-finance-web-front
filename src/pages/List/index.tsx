59f1e569c54035ca3962503ddf800480bf6133aa
Sidnei <sidneisonic@hotmail.com>
2024-03-18 22:59:24 -0300

    feat: agora usando useParamns para pegar o parametro da url e useMemo para verificar os ajustes da tela

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,12 +1,26 @@
+import { useParams } from "react-router-dom";
 import ContentHeader from "../../components/ContentHeader";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
 import { Container, Content, Filters } from "./styles";
+import { useMemo } from "react";
+
 
 const List = () => {
+    
+    const { type } = useParams();
+
+    const title = useMemo(() => {
+        return type === 'entry-balance' ? 'Entradas' : 'Saídas';
+    }, [type]);
+
+    const lineColor = useMemo(() => {
+        return type === 'entry-balance' ? '#4E41F0' : '#E44C4E';
+    }, [type]);
+
     return (
         <Container>
-            <ContentHeader title="Entradas" lineColor="#fff">
+            <ContentHeader title={title} lineColor={lineColor}>
                 <SelectInput options={[{value: 'teste', label: 'teste'}]} />
                 <SelectInput options={[{value: 'teste', label: 'teste'}]} />
             </ContentHeader>
